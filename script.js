24c62e52ad6741d88ac97c085df1e8b41b5e5019
Mauricio <mauparra80@hotmail.com>
2023-11-15 11:56:42 -0600

    finish "Read" toggle and adding books to dom/array

--- script.js
+++ script.js
@@ -21,6 +21,9 @@
 // title, author, pages
 //choose color, toggle read/not read
 
+const addBookBtn = document.querySelector("#submit");
+addBookBtn.addEventListener('click', saveUserInfo);
+
 let inputTitle = "book2";
 let inputAuthor = "Author2";
 let inputPages = 420;
@@ -38,13 +41,102 @@ function Book(title,author,pages,color,read) {
     this.read = read; //str or bool
 }
 
+function updateLibrary()
+{
+    
+}
+
+function openForm()
+{
+    document.querySelector(".form-container").style.display = "block";
+    document.querySelector(".form-blur").style.display = "block";
+}
+
+function toggleReadMenu()
+{
+    const readButton = document.querySelector('#input-read');
+    readButton.classList.toggle("toggle-read");
+    if (readButton.value == "Not Read")
+    {
+        readButton.value = "Read";
+    }
+    else {readButton.value = "Not Read"};
+}
+
+//called when submit button is clicked
+function saveUserInfo()
+{
+    inputTitle = document.getElementById("input-title").value;
+    inputAuthor = document.getElementById("input-author").value;
+    inputPages = document.getElementById("input-pages").value;
+    inputRead = document.getElementById("input-read").value;
+
+   
+
+    closeForm();
+    addCard();
+    addBookToLibrary();
+    updateClick();
+}
+
+//create object with current inputs and append new book object to library
+function addBookToLibrary()
+{
+    const book = new Book(inputTitle, inputAuthor, inputPages, "red", inputRead);
+    myLibrary.push(book);
+    console.log(book);
+    console.log(myLibrary);
+}
+
+function closeForm()
+{
+    document.querySelector(".form-container").style.display = "none";
+    document.querySelector(".form-blur").style.display = "none";
+    document.querySelector("form").reset();
+}
+
+
+//toggle read button when clicked
+function updateClick(){
+const readbtn = document.querySelectorAll(".book");
+readbtn.forEach((book, index) => {
+    book.querySelector("#read").addEventListener('click', () => {
+        changeReadBtn(index);
+    })
+})
+}
+
+function changeReadBtn(index)
+{
+    console.log(index); //this works
+    readBtn = document.querySelector("[data-index='"+index+"'] #read");
+
+    if (readBtn.textContent == "Not Read")
+    {
+        readBtn.style.backgroundColor = "rgb(108, 177, 108)";
+        readBtn.textContent = "Read";
+        myLibrary[index].read = "Read";
+        console.log(myLibrary[index]);
+    }
+    else if (readBtn.textContent == "Read")
+    {
+        
+        readBtn.style.backgroundColor = "rgb(255, 140, 140)";
+        readBtn.textContent = "Not Read";
+        myLibrary[index].read = "Not Read";
+        console.log(myLibrary[index]);
+    }
+}
+
 //creates dom card with user input and adds to tree
-function addBookToLibrary() {
+function addCard() {
     //get user input
     
+
     //create html content with user input
     let newBook = document.createElement("div");
     newBook.classList.add('book');
+    newBook.dataset.index = myLibrary.length;
     newBook.innerHTML = '<img src="./img/brown-book.png" alt="book">';
 
     let bookInfo = document.createElement("div");
@@ -66,29 +158,35 @@ function addBookToLibrary() {
     
     let bookButtons = document.createElement("div");
     bookButtons.classList.add('book-buttons');
-    bookButtons.innerHTML = '<button type="button" id="edit">Edit</button>';
+    bookButtons.innerHTML = '<button type="button" id="edit" data-index="' + myLibrary.length + '" ></button>';
     bookButtons.innerHTML += '<button type="button" id="read">Read</button>';
+    
     let pages = document.createElement('h4');
     pages.classList.add('pages');
     pages.textContent = inputPages;
 
+    // console.log(document.querySelector(".books .book:last-child button:last-child").textContent);
+
+
     bookBottom.appendChild(bookButtons);
     bookBottom.appendChild(pages);
     newBook.appendChild(bookBottom);
+
+    
     
     //append all to books
     document.querySelector('.books').appendChild(newBook);
+
+    const readbtn = document.querySelector(".books .book:last-child button:last-child");
+    
+    readbtn.textContent = inputRead;
+    
+
 }
 
-let newBook = document.getElementById('newBook');
-newBook.addEventListener('click', () => {
-    console.log("button clicked");
-    addBookToLibrary();
-});
 
-const norseMyth = new Book("Norse Mythology", "Neil Gaiman", 304, "blue", "read");
 
-console.log(norseMyth);
+
 
 
 
